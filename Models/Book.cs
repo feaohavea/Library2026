362f4b01d7f059c0011ebeaef1cb241cf6142c5e
F Havea <ac146751@avcol.school.nz>
2026-03-18 12:14:48 +1300

    Added genre table in models

--- Models/Book.cs
+++ Models/Book.cs
@@ -5,13 +5,13 @@
         public int BookID { get; set; }
         public string Title { get; set; }
         public string AuthorID { get; set; }
-        public string AuthorCode { get; set; } = string.Empty;
+        public string AuthorCode { get; set; } 
         public string Description { get; set; }
         public string SeriesID { get; set; }
         public string NoInSeries { get; set; }
         public int PublicationYear { get; set; }
         public string Genre { get; set; }
-        public string Status { get; set; } // Available, Checked Out, Reserved
+        public string Status { get; set; } // Available, Checked Out, Reserved - drop down in UI
         public string Location { get; set; } // Shelf location
         public string UserID { get; set; } // ID of the user who has checked out the book
 
---
+++ Models/Genre.cs
@@ -0,0 +1,8 @@
+﻿namespace Library2026.Models
+{
+    public class Genre
+    {
+        public int GenreID { get; set; }
+        public string GenreName { get; set; } 
+    }
+}
