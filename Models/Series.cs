e926c304eccc914cacc1a792defe97b9dcbd9480
F Havea <ac146751@avcol.school.nz>
2026-03-18 12:30:14 +1300

    Series

---
+++ Models/Series.cs
@@ -0,0 +1,10 @@
+﻿namespace Library2026.Models
+{
+    public class Series
+    {
+        public int SeriesID { get; set; }
+        public string SeriesName { get; set; }
+        public double SeriesNumbers { get; set; }
+        
+    }
+}
