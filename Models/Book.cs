3f691d4e3391c279f929668b1049165613707f0d
F Havea <ac146751@avcol.school.nz>
2026-03-25 12:23:10 +1300

    normalised the book table, deleted the scaffold, need to rescaffold.

--- Models/Book.cs
+++ Models/Book.cs
@@ -10,15 +10,14 @@
     {
         public int BookID { get; set; }
         public string Title { get; set; }
-        public string AuthorID { get; set; }
-        public string AuthorCode { get; set; } 
-        public string Description { get; set; }
-        public string SeriesID { get; set; }
-        public string NoInSeries { get; set; }
+        public int AuthorID { get; set; }
+        public int? SeriesID { get; set; }
+        public int? NoInSeries { get; set; }
         public int PublicationYear { get; set; }
-        public string Genre { get; set; }
-        public string Status { get; set; } // Available, Checked Out, Reserved - drop down in UI
-        public string Location { get; set; } // Shelf location
+        public int GenreID { get; set; }
+        public int StatusID { get; set; } // Available, Checked Out, Reserved - drop down in UI
+        public int LocationID { get; set; } // Shelf location
+        public string Description { get; set; }
 
     }
 }
