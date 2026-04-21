960063e78ff296130cb25eef544bfb1a05aafc5b
Fe'ao Havea <ac146751@avcol.school.nz>
2026-04-21 13:44:23 +1200

    deletng scaffolded controllers and views for a different, updated, book table that is better normalised. it meets the criteria for third normalisation.

--- Models/Book.cs
+++ Models/Book.cs
@@ -10,14 +10,11 @@
     {
         public int BookID { get; set; }
         public string Title { get; set; }
-        public int AuthorID { get; set; }
-        public int? SeriesID { get; set; }
-        public int? NoInSeries { get; set; }
-        public int PublicationYear { get; set; }
-        public int GenreID { get; set; }
+        public int Pages { get; set; }
         public int Status { get; set; } // Available, Checked Out, Reserved - drop down in UI
-        public int LocationID { get; set; } // Shelf location
         public string Description { get; set; }
+        public int LocationID { get; set; } // Shelf location
+
 
     }
 }
