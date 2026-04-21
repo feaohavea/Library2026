3f6074c9a4b558dfe216778af3463c97026994ea
Fe'ao Havea <ac146751@avcol.school.nz>
2026-04-22 11:56:27 +1200

    changed the location table to just an enum on the book table so that i wouldnt require a separate table for the location and a potential junction table for the book and location. this cuts down on my table numbers and makes it easier to manage.

--- Models/Book.cs
+++ Models/Book.cs
@@ -6,6 +6,10 @@
         CheckedOut,
         Reserved
     }
+    public enum Shelf
+    {
+        A, B, C, D, E, F, G, H, I, J
+    }
     public class Book
     {
         public int BookID { get; set; }
@@ -13,13 +17,12 @@
         public int Pages { get; set; }
         public int Status { get; set; } // Available, Checked Out, Reserved - drop down in UI
         public string Description { get; set; }
-        public int LocationID { get; set; } // Shelf location
+        public int Location { get; set; } // Shelf location
 
         public ICollection<AuthorBook> AuthorBooks { get; set; }
         public ICollection<GenreBook> GenreBooks { get; set; }
         public ICollection<SeriesBook> SeriesBooks { get; set; }
         public ICollection<UserBook> UserBooks { get; set; }
-
 
     }
 }
